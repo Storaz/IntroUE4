3a0104c23c43159d9e29fbf3641fb3f6d92e0e2b
Florian <f.herrmann@ludus-academie.com>
2018-10-11 14:47:50 +0200

    Intro

---
+++ Source/IntroUE4/IntroUE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IntroUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IntroUE4, "IntroUE4" );
